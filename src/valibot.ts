cc7d4b0b154ddeea54c6d4106318458d376dd535
Camron Flanders <me@camronflanders.com>
2024-12-06 08:48:20 -0600

    remove unused?

--- src/valibot.ts
+++ src/valibot.ts
@@ -1,16 +1,8 @@
 import { toJsonSchema } from "@valibot/to-json-schema";
-import type { ValidationTargets } from "hono";
-import {
-  type BaseIssue,
-  type BaseSchema,
-  type GenericSchema,
-  type GenericSchemaAsync,
-  parseAsync,
-} from "valibot";
+import { type BaseIssue, type BaseSchema, parseAsync } from "valibot";
 
 import { convert } from "./toOpenAPISchema/index.ts";
-import type { OpenAPIRouteHandlerConfig, ResolverResult } from "./types.ts";
-import { generateValidatorDocs } from "./utils.ts";
+import type { ResolverResult } from "./types.ts";
 
 export function resolver<
   T extends BaseSchema<unknown, unknown, BaseIssue<unknown>>,
@@ -25,13 +17,4 @@ export function resolver<
       await parseAsync(schema, value);
     },
   };
-}
-
-export function validator<
-  T extends GenericSchema | GenericSchemaAsync,
-  Target extends keyof ValidationTargets,
->(target: Target, schema: T) {
-  return async (config: OpenAPIRouteHandlerConfig) =>
-    // @ts-expect-error Need to fix the type
-    generateValidatorDocs(target, await resolver(schema).builder(config));
 }
