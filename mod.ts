61bf695386c34bffbe55c6a6fbd6c2a8cab40c0b
Camron Flanders <me@camronflanders.com>
2025-02-19 11:13:50 -0600

    try exporting types again

--- mod.ts
+++ mod.ts
@@ -1,2 +1,5 @@
-export { describeRoute } from "./src/route.ts";
+// @ts-expect-error: need to fix the import requirements mismtach between jsr/esm
 export { getOpenAPISpecs } from "./src/openapi.ts";
+// @ts-expect-error: need to fix the import requirements mismtach between jsr/esm
+export { describeRoute } from "./src/route.ts";
+export type * from "./src/types.ts";
